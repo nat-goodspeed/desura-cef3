9e7c4284e801ce72b556d9185713892b2b5c494a
Lodle <mchandler@lindenlab.com>
2014-04-01 13:17:05 -0700

    Added auto download of cef3

--- build_cef.bat
+++ build_cef.bat
@@ -1,3 +1,13 @@
+if NOT EXIST cef3 (
+	IF NOT EXIST build_cef3 (
+		mkdir build_cef3
+	)	
+	cd build_cef3
+	cmake ..\cef3_dl -G "NMake Makefiles"
+	nmake
+	cd ..
+)
+
 IF NOT EXIST build_vis (
   mkdir build_vis
 )
--- build_cef.sh
+++ build_cef.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+if [ ! -d "cef3" ]; then
+	if [ ! -d "build_cef3" ]; then
+		mkdir build_cef3
+	fi
+	cd build_cef3
+	cmake ..\cef3_dl
+	make
+	cd ..
+fi
+
 if [ ! -d "build_nix" ]; then
 	mkdir build_nix
 fi
